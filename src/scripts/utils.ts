689fa0935d4def315d103a3e33b468bea5455309
Aboufares <faresservices94@gmail.com>
2022-08-01 15:57:07 +0200

    Initial commit

---
+++ src/scripts/utils.ts
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2021, Oracle and/or its affiliates.
+ * Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+ */
+
+declare let process: any;
+
+/**
+ * Function to take a date and return it as a formatted string.
+ *
+ * @param {Date} date the date to format into a nice string
+ */
+export function dateToMDY(date: string) {
+  const dateObj = new Date(date);
+  const formattedDate = dateObj.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+  return formattedDate;
+}
+
+/**
+ * When authorization is needed for images, change the image URL so that it
+ * goes to this application's Express server in order for the authorization
+ * headers are added to the request.
+ *
+ * See the following files where proxying is setup/done
+ * - 'src/scripts/server-config-utils.getClient' for the code proxying requests for content
+ * - 'src/server/server' for the Express server proxying.
+ *
+ * @param String originalUrl the image's original url
+ */
+export function getImageUrl(originalUrl) {
+  if (process.env.AUTH || process.env.AUTH_PARAMS) {
+    // strip off the server URL from the front of the URL to make a relative URL
+    // causing the request to go to this application's Express server
+    const url = new URL(originalUrl);
+    return url.pathname + url.search;
+  }
+  return originalUrl;
+}
